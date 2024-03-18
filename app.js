fde244608ec5c46dd346e6b800e19dcc705e6a25
Mandrusian <143601569+Mandrusian@users.noreply.github.com>
2024-03-19 07:29:36 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-document.onreadystatechange = function() {
-            if (document.readyState !== "complete") {
-                document.querySelector(
-                  "body").style.visibility = "hidden";
-                document.querySelector(
-                  "#spinner").style.visibility = "visible";
-            } else {
-                document.querySelector(
-                  "#spinner").style.display = "none";
-                document.querySelector(
-                  "body").style.visibility = "visible";
-            }
-       };
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
